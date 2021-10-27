f43e5edde516efaa79a9ed917a9a64015d098d98
Moazzam Adil Khan <moaadi3890@gmail.com>
2021-10-27 08:26:00 +0500

    Convert SQLite database to Postgre DB.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 gravatar = Gravatar(app,
                     size=100,
@@ -26,7 +26,7 @@ gravatar = Gravatar(app,
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite://blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
